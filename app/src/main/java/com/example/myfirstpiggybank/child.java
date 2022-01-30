3f82a61cd20dae570c7b6bb602ffafa5bc2faa50
Pranav-Pra <68164506+Pranav-Pra@users.noreply.github.com>
2022-01-30 15:33:35 -0600

    signout

--- app/src/main/java/com/example/myfirstpiggybank/child.java
+++ app/src/main/java/com/example/myfirstpiggybank/child.java
@@ -2,13 +2,27 @@ package com.example.myfirstpiggybank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class child extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
+    }
+
+    public void signOut(View v) {
+        mAuth.getInstance().signOut();
+
+        Intent welcomeIntent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(welcomeIntent);
+
     }
 }
