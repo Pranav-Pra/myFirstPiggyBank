c37220a8f232f737898028ab856952fdf672ede0
meganw0009 <meganhr08@gmail.com>
2022-01-30 19:15:32 -0600

    this app is making me cry

--- app/src/main/java/com/example/myfirstpiggybank/ChildChores.java
+++ app/src/main/java/com/example/myfirstpiggybank/ChildChores.java
@@ -18,41 +18,38 @@ public class ChildChores extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_chores);
-//
-//        Chore choreOne = new Chore("sweep", "use broom on floor", 5);
-//        Chore choreTwo = new Chore("brush", "brush brush", 10);
-//        chores.add(choreOne);
-//        chores.add(choreTwo);
+
+        Chore choreOne = new Chore("sweep", "use broom on floor", 5);
+        Chore choreTwo = new Chore("brush", "brush brush", 10);
+        chores.add(choreOne);
+        chores.add(choreTwo);
 
         LinearLayout root = (LinearLayout) findViewById(R.id.root);
 
-        LinearLayout chore = new LinearLayout(this);
-        chore.setOrientation(LinearLayout.HORIZONTAL);
+      for (int i = 0; i < chores.size(); i++) {
 
-//      for (int i = 0; i < chores.size(); i++) {
-//
-//            LinearLayout chore = new LinearLayout(this);
-//            chore.setOrientation(LinearLayout.HORIZONTAL);
-//
-//            TextView choreName = new TextView(this);
-//            choreName.setText("peepers");
-//
-//            TextView choreDesc = new TextView(this);
-//            choreDesc.setText(chores.get(i).getDesc());
-//
-//            TextView choreValue = new TextView(this);
-//            choreValue.setText(chores.get(i).getValue());
-//
-//            Button completeTaskButton = new Button(this);
-//            completeTaskButton.setText("Finished Chore!");
-//
-//            chore.addView(choreName);
+            LinearLayout chore = new LinearLayout(this);
+            chore.setOrientation(LinearLayout.HORIZONTAL);
+
+            TextView choreName = new TextView(this);
+            choreName.setText(chores.get(i).getName().toString());
+
+            TextView choreDesc = new TextView(this);
+            choreDesc.setText(chores.get(i).getDesc());
+
+            TextView choreValue = new TextView(this);
+            choreValue.setText(chores.get(i).getValue());
+
+            Button completeTaskButton = new Button(this);
+            completeTaskButton.setText("Finished Chore!");
+
+            chore.addView(choreName);
 //            chore.addView(choreDesc);
 //            chore.addView(choreValue);
 //            chore.addView(completeTaskButton);
-//
-//            root.addView(chore);
-//        }
+
+            root.addView(chore);
+        }
 
     }
 }
